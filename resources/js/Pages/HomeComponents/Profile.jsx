57eef0af7eb954e8a5989c10906cabf107dfec1e
faracutrisfa <faracutrisfa@gmail.com>
2024-10-26 12:45:14 +0700

    style: add style some components at home page

--- resources/js/Pages/HomeComponents/Profile.jsx
+++ resources/js/Pages/HomeComponents/Profile.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Profile = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <section className='lg:px-20 px-10'>
+            ini profil
+        </section>
+    )
 }
 
 export default Profile
