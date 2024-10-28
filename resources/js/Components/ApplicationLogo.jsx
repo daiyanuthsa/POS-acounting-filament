0057751df908f8d6d84a63c94cc88cfc782a1a5d
faracutrisfa <faracutrisfa@gmail.com>
2024-10-28 17:20:07 +0700

    style: add style navbar

---
+++ resources/js/Components/ApplicationLogo.jsx
@@ -0,0 +1,12 @@
+import { Link } from '@inertiajs/react'
+import React from 'react'
+
+const ApplicationLogo = () => {
+    return (
+        <Link className="font-bold transition duration-300 ease-in-out transform hover:scale-105 active:scale-95 font-dmsans">
+            LOGO
+        </Link>
+    )
+}
+
+export default ApplicationLogo
