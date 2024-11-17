394f94fe683546e05070b82cf921533b1f98ae69
faracutrisfa <faracutrisfa@gmail.com>
2024-11-17 20:04:11 +0700

    style(report): add style report components

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,9 @@
     <link
         href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
         rel="stylesheet">
+    <link 
+        href="https://fonts.googleapis.com/css2?family=Anonymous+Pro:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        rel="stylesheet">
     @viteReactRefresh
     @vite('resources/js/app.jsx')
     @inertiaHead
