1f05b7717712d0b977a3d4b3bd28d45a7d0121f1
faracutrisfa <faracutrisfa@gmail.com>
2024-11-28 15:09:25 +0700

    feat(data): update dataProduk

--- resources/js/Data/DataProduk.js
+++ resources/js/Data/DataProduk.js
@@ -1,18 +1,19 @@
 const DataProduk = [
     {
-        img: './images/home/Wisata1.webp',
-        title: 'UMKM Pengolahan Kopi',
-        description: 'Manggarai Timur terkenal dengan produksi kopi yang berkualitas, terutama jenis Robusta dan Arabika. Setiap tegukan kopi bukan hanya memberikan kelezatan, tetapi juga mendukung kehidupan para petani kopi lokal yang penuh dedikasi.',
+        img: './images/home/UMKM1.webp',
+        title: 'Warung dan Toko kecil',
     },
     {
-        img: './images/home/Wisata1.webp',
-        title: 'UMKM Tenun Ikat',
-        description: 'Seni Tenun Ikat adalah kebanggaan Manggarai Timur. Setiap helai kain tenun ikat dibuat dengan teliti dan penuh makna, menjadikannya produk yang istimewa dan eksklusif untuk dikenakan atau dijadikan suvenir khas daerah.',
+        img: './images/home/UMKM2.webp',
+        title: 'Pengusaha makanan',
     },
     {
-        img: './images/home/Wisata1.webp',
-        title: 'UMKM Kerajinan Tangan',
-        description: 'UMKM di Manggarai Timur juga dikenal dengan berbagai kerajinan tangan. Dari anyaman bambu, kerajinan kayu, hingga perhiasan tradisional, para pengrajin lokal mencurahkan keahlian mereka untuk menciptakan karya yang memikat. ',
+        img: './images/home/UMKM3.webp',
+        title: 'Pengerajin lokal',
+    },
+    {
+        img: './images/home/UMKM4.webp',
+        title: 'Jasa kecil',
     },
 ]
 
